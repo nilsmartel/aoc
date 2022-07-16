4cf8c51bffb4b5ed089b0af747d84cde1023c401
Nils Martel <nilsmartel@yahoo.de>
2022-07-16 18:27:44 +0200

    Implement day2

--- 2021/day2/src/day2/core.clj
+++ 2021/day2/src/day2/core.clj
@@ -8,12 +8,27 @@
 
 (defn parse [text] (map parseLine (str/split text #"\n")))
 
-(defn position [instructions] (reduce (fn [[h d] {:keys dir value}]
+(defn position [instructions] (reduce (fn [[h d] {dir :dir value :value}]
                                         (match [dir]  ["forward"] [(+ h value) d]
                                                ["down"] [h (+ d value)]
                                                ["up"] [h (- d value)])) [0 0] instructions))
 
+(defn position2 [instructions] (reduce (fn [[h d aim] {dir :dir value :value}]
+                                        (match [dir]
+                                               ["forward"] [(+ h value) (+ d (* aim value)) aim]
+                                               ["down"] [h d (+ aim value)]
+                                               ["up"] [h d (- aim value)]))
+                                       [0 0 0] instructions))
+
 (defn -main
   [input & _args]
-  (let [data (-> input slurp parse)]
-    (print (data))))
+  (let [data (-> input slurp parse)
+        [h d] (position data)
+        [h2 d2 _aim] (position2 data)
+        ]
+    (println "horizontal" h)
+    (println "depth" d)
+    (println "multiplied" (* h d))
+    (println "-----")
+    (println "multiplied" (* h2 d2))
+    ))
