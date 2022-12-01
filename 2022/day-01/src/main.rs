1cc251fc9a08d545233da457538d55f934a03895
Nils Martel <nilsmartel@yahoo.de>
2022-12-01 12:23:40 +0100

    Get second star

--- 2022/day-01/src/main.rs
+++ 2022/day-01/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let input = std::fs::read_to_string("../input/input-text-01").unwrap();
 
-    let max_calories = input
+    // Calories carried per elf
+    let mut calories = input
         .split("\n\n")
         // Useful to inspect individual groups
         // .inspect(|g| {
@@ -15,8 +16,13 @@ fn main() {
                 .map(|line| line.parse::<u64>().expect("valid integer string"))
                 .sum::<u64>()
         })
-        .max()
-        .unwrap();
+        .collect::<Vec<_>>();
 
-    println!("{max_calories}");
+    calories.sort_unstable();
+
+    let max = calories[0];
+    let top3 = calories[..3].iter().sum::<u64>();
+
+    println!("max:   {max}");
+    println!("top 3: {top3}");
 }
